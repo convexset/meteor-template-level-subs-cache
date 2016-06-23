5baa3a977b8d9a99937ee66cb43dc4339a45b59a
Jeremy Chen <convexset@gmail.com>
2016-06-23 14:59:00 +0800

    fixed Meteor.setTimeout issue of ccorcos:subs-cache by using alternative package with fix

--- packages/template-level-subs-cache/package.js
+++ packages/template-level-subs-cache/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'convexset:template-level-subs-cache',
-	version: '0.1.4_5',
+	version: '0.1.4_6',
 	summary: 'A template-level subscriptions cache providing for reactive parameters',
 	git: 'https://github.com/convexset/meteor-template-level-subs-cache',
 	documentation: '../../README.md'
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
 		[
 			'reactive-var', 'reactive-dict',
 			'blaze-html-templates',
-			'ccorcos:subs-cache@0.1.0'
+			'jimmiebtlr:subs-cache@0.1.0'  // fix for a Meteor.setTimeout issue (https://github.com/ccorcos/meteor-subs-cache/issues/10); was: 'ccorcos:subs-cache@0.1.0'
 		],
 		'client'
 	);
